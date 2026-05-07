cfd7ae5aa0ac64c07c6d880fac15f791da03dc94
dwaris <dwaris@itclowd.de>
2026-05-07 19:36:01 +0200

    BATMAN

---
+++ exercises/04_primitive_types/primitive_types5.rs
@@ -0,0 +1,6 @@
+fn main() {
+    let cat = ("Furry McFurson", 3.5);
+    let (name, age) = cat;
+
+    println!("{name} is {age} years old");
+}
---
+++ exercises/17_tests/tests1.rs
@@ -0,0 +1,21 @@
+// Tests are important to ensure that your code does what you think it should
+// do.
+
+fn is_even(n: i64) -> bool {
+    n % 2 == 0
+}
+
+fn main() {
+    // You can optionally experiment here.
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::is_even;
+
+    #[test]
+    fn you_can_assert() {
+        assert!(is_even(42));
+        assert!(!is_even(23));
+    }
+}
